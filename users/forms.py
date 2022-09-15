92f5a3561de1af5db57c2851567ddb4a2855c296
Mayphiga Llewellyn <6310610982@engr.tu.ac.th>
2022-09-15 19:27:28 +0700

    login page

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,16 @@
+from dataclasses import field
 from django.forms import ModelForm
 from .models import Order
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = ["course"]
+        
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        field = ['username', 'email', 'password1', 'password2']
